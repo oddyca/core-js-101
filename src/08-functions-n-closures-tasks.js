4852d5c768b7a0a4027a2514ad5fe03b688ea7e0
Siarhei Prakapovich <prokser27@icloud.com>
2022-12-25 01:27:36 +0300

    refactor: replace a regular function with an arrow one in functions-n-closures task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -45,9 +45,7 @@ function getComposition(/* f, g */) {
  *
  */
 function getPowerFunction(exponent) {
-  return function (x) {
-    return x ** exponent;
-  };
+  return (x) => x ** exponent;
 }
 
 
